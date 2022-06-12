ee84b76465548e050640a23f3423492811797b1d
oscar9181 <oscarokola17@gmail.com>
2022-06-12 11:26:34 +0300

    add urlpatterns

--- awwwards/urls.py
+++ awwwards/urls.py
@@ -1,2 +1,7 @@
-from django.urls import path
+from django.urls import URLPattern, path
 from . import views
+
+urlpatterns = [
+    path('',views.home,name='home'),
+    path('portfolio/',views.portfolio,name='portfolio')
+]
