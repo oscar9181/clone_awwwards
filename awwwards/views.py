16250099857b6b6fc03fa76f36f0b1f67618504f
oscar9181 <oscarokola17@gmail.com>
2022-06-12 19:27:57 +0300

    register and login page

--- awwwards/views.py
+++ awwwards/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 
+
+
+def register(request):
+    return render(request,'awwards/register.html')
+
+
+def login(request):
+    return render(request,'awwards/login.html')
+
+
 def home (request):
     
     return render(request,'awwards/home.html')
