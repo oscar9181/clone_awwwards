a3ed30726102a9f1d95ccc74302b1674fe82017b
oscar9181 <oscarokola17@gmail.com>
2022-06-12 11:20:05 +0300

    render template

--- awwwards/views.py
+++ awwwards/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home (request):
+    return render(request,'awwards/home.html')
+
+def portfolio (request):
+    return render(request,'awwards/portfolio.html')
