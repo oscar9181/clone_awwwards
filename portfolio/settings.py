e79255e4686fc8d92f8dd26f0b9e02d32f22c13a
oscar9181 <oscarokola17@gmail.com>
2022-06-15 13:39:09 +0300

    deploying to heroku

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
-import django_heroku
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -28,8 +26,7 @@ SECRET_KEY = 'django-insecure-r-j$yit3e&=82&poyb1gaet-lr+qp06ji!fr6v&=(i@m@v9amo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -140,10 +137,7 @@ STATICFILES_DIRS = (
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-django_heroku.settings(locals())
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
-
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
